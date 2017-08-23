79fbd28d98455362263a58f37c15b9d3e4ae0eab
Nate Cornell <natec@rb-com.com>
2017-08-23 08:07:43 -0700

    Adds search and traverse functions

--- src/BinarySearchTree.php
+++ src/BinarySearchTree.php
@@ -2,16 +2,72 @@
 
 namespace DataStructures;
 use BinarySearchTreeNode;
-use Stack;
+use \Ds\Stack;
+use \Ds\Queue;
 
 class BinarySearchTree {
     public $head;
 
-    public function __construct(BinarySearchTreeNode $head=null) {
+    public function __construct(\DataStructures\BinarySearchTreeNode $head=null) {
         $this->head = $head;
     }
 
-    public function getMin() {
+    public function depthFirstSearch(int $needle, bool $depthFirst=true) {
+        if (! $this->head) { return null; }
+
+        $toVisit = ($depthFirst) ? new Stack() : new Queue();
+        $toVisit->push($this->head);
+
+        while (! $toVisit->isEmpty()) {
+            $current = $toVisit->pop();
+
+            if ($current->value == $needle) {
+                return $current;
+            }
+
+            if ($current < $needle && $current->right != null) { 
+                $toVisit->push($current->right);
+            }
+
+            if ($current > $needle && $current->left != null) {
+                $toVisit->push($current->left);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns an array of the values in the order that the tree
+     * was traversed.
+     *
+     * @param bool $depthFirst  Traverse in a depth-first fashion (default).
+     *                          If false, will perform breadth-first traverse.
+     *
+     * return array(int)
+     */
+    public function traverse(bool $depthFirst=true) {
+        if ($this->head === null) { return false; }
+
+        $toVisit = ($depthFirst) ? new Stack() : new Queue();
+        $toVisit->push($this->head);
+        $visited = [];
+
+        while (! $toVisit->isEmpty()) {
+            $current = $toVisit->pop();
+
+            if ($current->left !== null) {
+                $toVisit->push($current->left);
+            }
+
+            if ($current->right !== null) {
+                $toVisit->push($current->right);
+            }
+
+            $visited[] = $current->value;
+        }
+
+        return $visited;
     }
 }
 
