c211c5cf3199f79b996b5374cbdd486785569653
Nate Cornell <natec@rb-com.com>
2017-08-23 08:07:25 -0700

    Adds 'add' function

--- src/BinarySearchTreeNode.php
+++ src/BinarySearchTreeNode.php
@@ -7,11 +7,24 @@ class BinarySearchTreeNode {
     public $left;
     public $right;
 
-    public function __construct(int $value, BinarySearchTreeNode $left=null, 
-            BinarySearchTreeNode $right=null) {
+    public function __construct(int $value, \DataStructures\BinarySearchTreeNode $left=null, 
+            \DataStructures\BinarySearchTreeNode $right=null) {
         $this->value = $value;
         $this->left  = $left;
         $this->right = $right;
+    }
+
+    public function add(\DataStructures\BinarySearchTreeNode $child) {
+        $position = ($child->value <= $this->value) ? 'left' : 'right';
+
+        if ($this->$position === null) {
+            $this->$position = $child;
+        } else {
+            $this->$position->add($child);
+        }
+    }
+
+    public function remove($value) {
     }
 }
 
